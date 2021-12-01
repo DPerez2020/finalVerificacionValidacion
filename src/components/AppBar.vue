ba300f04edb0a08b8b412cee2d733caa95ea78d3
Dawin Rafael Perez Batista <dawin2020@hotmail.com>
2021-12-01 06:06:28 -0400

    Se agregaron los iconos de fonr awesome y se comenzo a estilizar la pagina principal

---
+++ src/components/AppBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <v-app-bar
+      color="#1D4F91"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>
+          <v-img src="https://xapi.unapec.edu.do/logo/logo.png"></v-img>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>fas fa-cog</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>fas fa-user</v-icon>
+      </v-btn>
+      <v-btn>
+        Dawin Rafael Perez Batista
+      </v-btn>      
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+  </div>
+</template>
+<script>
+export default {
+    name:"AppBar"
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,35 @@
 <template>
-  <hello-world />
+  <div>
+    <v-container>
+    <h2>Informacion de inscripcion</h2>
+      <v-tabs v-model="tab">
+        <v-tab>
+          Buscar horario            
+        </v-tab>
+        <v-tab>
+          Inscripciones activas
+        </v-tab>        
+      </v-tabs>
+      <v-tabs-items v-model="tab">
+            <v-tab-item>
+              ITEM1
+            </v-tab-item>
+                      <v-tab-item>
+            ITEM2
+          </v-tab-item>
+            </v-tabs-items>                  
+    </v-container>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'Home',
-
-    components: {
-      HelloWorld,
+    data() {
+      return {
+        tab:null
+      }
     },
   }
 </script>
